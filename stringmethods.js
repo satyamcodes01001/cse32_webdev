1a55b0379919d6c4154f43dbd01db0ebda6ff73c
Satyam Seth <satyamcodes01001@gmail.com>
2026-07-29 10:17:15 +0530

    added to repo

---
+++ stringmethods.js
@@ -0,0 +1,13 @@
+let text="javascript programming";
+console.log(text.length);
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+console.log(text.includes("hello"));
+console.log(text.startsWith("java"));
+console.log(text.endsWith("ing"));
+console.log(text.slice(0));
+console.log(text.replace("javascript","World"));
+console.log(text.slice(0,5));
+console.log(text.substring(0,5));
+
+console.log(text.split(" "));
